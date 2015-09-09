f7cd7b2ced642c6c63407051a3aab04628c79ada
Japneet <japneets94@gmail.com>
2015-09-09 14:46:03 -0400

    Revert "commit 2"
    This reverts commit c9b0396e57d78a8f0b141075121bb463f7644dc6.

--- sample3.java
+++ sample3.java
@@ -1,7 +1,6 @@
 public class Sample{
 	public static void main(String[]args){
 		System.out.println("Hello");
-		System.out.println("I HATE THIS CLASS");
-		System.out.println(":D")
+		System.out.println("I HATE THIS CLASS")
 	}
 }
