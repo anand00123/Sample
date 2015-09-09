c9b0396e57d78a8f0b141075121bb463f7644dc6
Japneet <japneets94@gmail.com>
2015-09-09 14:38:44 -0400

    commit 2

--- sample3.java
+++ sample3.java
@@ -1,6 +1,7 @@
 public class Sample{
 	public static void main(String[]args){
 		System.out.println("Hello");
-		System.out.println("I HATE THIS CLASS")
+		System.out.println("I HATE THIS CLASS");
+		System.out.println(":D")
 	}
 }
